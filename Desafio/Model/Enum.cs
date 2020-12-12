4748b619bb5a8a798868f1e57ab491b41ab9027e
Lucas Sandim <lucasandim_gd1@hotmail.com>
2020-12-11 21:43:35 -0400

    adicionando projeto API e projeto de Testes

---
+++ Desafio/Model/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Model
+{
+    public class Enum
+    {
+        #region StatusFamilia
+        /// <summary>     Valores que representam o status de uma Família </summary>
+        ///
+        /// <remarks>   Lucas, 11/12/2020. </remarks>
+        #endregion
+        public enum StatusFamilia : int
+        {
+            CADASTRO_VALIDO = 0,
+            TEM_CASA = 1,
+            JA_SELECIONADA = 2,
+            CADASTRO_INCOMPLETO = 3,
+        }
+    }
+}
---
+++ Desafio/Model/Familia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Model
+{
+    public class Familia
+    {
+        public int Id { get; set; }
+        public int RendaTotal { get; set; }
+        public Enum.StatusFamilia Status { get; set; }
+        public Pretendente Pretendente { get; set; }
+    }
+}
